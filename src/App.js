a5aa96fd31651ed0f63a161d92ae62ebdf947b36
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-09-29 11:12:43 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Activities from './components/Activities/Activities';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="container">
+      <Header></Header>
       <Activities></Activities>
     </div>
   );
